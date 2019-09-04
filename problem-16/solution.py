2052cda79ce52b96799fa6d68c936dadd0bc6b33
Bharath Narayan M G <bharath144@gmail.com>
2019-09-04 08:46:20 +0530

    Update solution.py

--- problem-16/solution.py
+++ problem-16/solution.py
@@ -1,10 +1,25 @@
 from math import floor, pow
 
-least_sum = 0
-num = pow(2, 15)
-num = list(str(num))
-for i in num:
-    least_sum = least_sum + int(i)
+def calculate_sum(power):
+    sum = 0
+    num = floor(pow(2, power))
+    num = list(str(num))
+    for i in num:
+        sum = sum + int(i)
+    
+    return sum
 
 def calclulate_power_digit_sum(power):
-    multiplier = power / 15
+    smallest = 15
+    least_sum = calculate_sum(smallest)
+    
+    multiplier = floor(power / smallest)
+    fractional_multiplier = floor(power % smallest)
+
+    pd_sum = least_sum * multiplier
+    pd_sum = pd_sum * floor(calculate_sum(fractional_multiplier))
+
+    print(least_sum)
+    print(pd_sum)
+
+print(calculate_sum(1000))
